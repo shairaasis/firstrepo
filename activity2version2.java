be03ff9195783911d808af694c059ff7d43cd645
Shaira Asis <shairaasis99@gmail.com>
2022-04-07 22:51:51 +0800

    added activities

--- activity2version2.java
+++ activity2version2.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;  
-public class activity2{  
+public class activity2version2{  
     public static void main(String[] args)   
     {  
         Scanner sc=new Scanner(System.in);  
         //creates arrays person and age in the memory of length 5
-        String[] persons = new String[5];
-        int[] age = new int[5];
+        int length;
 
-        System.out.println("Please enter upto five names with their ages.");
-        System.out.println("---------------------------------------------");
+        System.out.println("Enter the number of persons you want to store in the array: ");   
+        length=sc.nextInt();
+        String[] persons = new String[length];
+        int[] age = new int[length];
+        sc.nextLine();
+        System.out.println("Please enter the names with their ages.");
+        System.out.println("---------------------------------------");
         
         //for loop to ask user to input upto 5 names with their age
-        for(int index=0; index<persons.length; index++)  
+        for(int count=0; count<persons.length; count++)  
         {  
             // Reading user input and store it in arrays person and age
             System.out.println("Enter the name: ");   
-            persons[index]=sc.nextLine();
+            persons[count]=sc.nextLine();
             System.out.println("Enter the age: ");    
-            age[index]=sc.nextInt();
+            age[count]=sc.nextInt();
             sc.nextLine();
         }  
 
@@ -25,7 +29,7 @@ public class activity2{
         System.out.println("----------------------------------------------------");
 
         //for loop to access the elements inside the arrays persons and age then display them
-        for (int index=0; index < persons.length; index+=1){
+        for (int index=0; index < persons.length; index++){
             System.out.println("Name: " + persons[index] +  "           Age: " + age[index]);
         }
 
